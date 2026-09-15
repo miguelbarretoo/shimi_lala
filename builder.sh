00602a621ec39430a7db4fff3968cc09257132ac
Miguel Barreto <miguel03barreto@gmail.com>
2026-09-14 23:40:10 -0300

    Refactor builder.sh for improved package setup
    Updated package installation and removed unnecessary commands.

--- builder.sh
+++ builder.sh
@@ -1,20 +1,11 @@
+# Define device (change if necessary)
+DEVICE=c2s
+
 # Update repos
 sudo apt update
 
 # Install dependencies
-sudo apt install bc bison build-essential kmod ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
-
-# setup repo
-mkdir ~/bin
-
-# make path
-PATH=~/bin:$PATH
-
-# download repo
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-
-# make repo executable
-chmod a+x ~/bin/repo
+sudo apt update && sudo apt install -yq attr bc bison build-essential ccache clang curl ffmpeg flex g++ g++-aarch64-linux-gnu g++-multilib gcc-aarch64-linux-gnu gcc-multilib git git-lfs gnupg gperf imagemagick kmod lld lib32readline-dev lib32z1-dev libbrotli-dev libbz2-dev libgtest-dev liblz4-dev liblz4-tool libncurses5-dev libpcre2-dev libprotobuf-dev libssl-dev libunwind-dev libxml2 libxml2-utils libzstd-dev linux-modules-extra-$(uname -r) libsdl1.2-dev lzop pngcrush protobuf-compiler python-is-python3 rsync schedtool squashfs-tools webp wget xsltproc zip zlib1g-dev && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && source "$HOME/.cargo/env" && rustc --version && cargo --version && wget https://raw.githubusercontent.com/kdrag0n/kramflash/refs/heads/master/mkbootimg.py && sudo ln -sf "$(pwd)/mkbootimg.py" /usr/bin/mkbootimg && sudo chmod +x /usr/bin/mkbootimg && sudo modprobe erofs f2fs
 
 # setup git account
 git config --global user.name NexusBR-odessa
@@ -22,17 +13,16 @@ git config --global user.name NexusBR-odessa
 # setup email
 git config --global user.email miguel03barreto@gmail.com
 
-# make dir
-mkdir lineage2
+# Clone source
+git clone https://github.com/Ats0c/UN1CA_y2slte --recurse-submodules
 
-# cd
-cd lineage2
+# Remove sudo references:
+sed -i '104,111d' scripts/extract_fw.sh
+sed -i '13,20d' platform/exynos990/patches/tethering_legacy/customize.sh
 
-# init repo (obs: you can edit this in case if you want another rom)
-repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
+# Initialize build enviroment
+cd UN1CA_y2slte && source buildenv.sh $DEVICE
 
-# clone manifest
-git clone https://github.com/NexusBR-odessa/local_manifests_x1s.git -b main .repo/local_manifests
+# Start build
+./scripts/make_rom.sh
 
-# sync
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync
