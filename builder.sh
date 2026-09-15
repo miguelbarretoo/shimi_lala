b1726782de909b067fd10f1756e90c1e9a5d074a
Miguel Barreto <miguel03barreto@gmail.com>
2026-09-15 01:11:12 -0300

    Update dependencies in builder.sh
    Replaced 'brotli' with 'patchelf' in the installation command.

--- builder.sh
+++ builder.sh
@@ -5,7 +5,7 @@ DEVICE=${1:-c2s}
 sudo apt update
 
 # Install dependencies
-sudo apt update && sudo apt install -yq attr bc bison build-essential 7zip brotli openjdk-21-jdk ccache clang curl ffmpeg flex g++ g++-aarch64-linux-gnu gcc-aarch64-linux-gnu curl git git-lfs gnupg gperf imagemagick kmod lld lib32readline-dev lib32z1-dev libbrotli-dev libbz2-dev libgtest-dev liblz4-dev liblz4-tool libncurses5-dev libpcre2-dev libprotobuf-dev libssl-dev libunwind-dev libxml2 libxml2-utils libzstd-dev linux-modules-extra-$(uname -r) libsdl1.2-dev lzop pngcrush protobuf-compiler python-is-python3 rsync schedtool squashfs-tools webp wget xsltproc zip zlib1g-dev
+sudo apt update && sudo apt install -yq attr bc bison build-essential 7zip brotli patchelf openjdk-21-jdk ccache clang curl ffmpeg flex g++ g++-aarch64-linux-gnu gcc-aarch64-linux-gnu curl git git-lfs gnupg gperf imagemagick kmod lld lib32readline-dev lib32z1-dev libbrotli-dev libbz2-dev libgtest-dev liblz4-dev liblz4-tool libncurses5-dev libpcre2-dev libprotobuf-dev libssl-dev libunwind-dev libxml2 libxml2-utils libzstd-dev linux-modules-extra-$(uname -r) libsdl1.2-dev lzop pngcrush protobuf-compiler python-is-python3 rsync schedtool squashfs-tools webp wget xsltproc zip zlib1g-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 wget https://raw.githubusercontent.com/kdrag0n/kramflash/refs/heads/master/mkbootimg.py && sudo ln -sf $(pwd)/mkbootimg.py /usr/bin/mkbootimg && chmod +x /usr/bin/mkbootimg
