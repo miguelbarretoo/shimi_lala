9a75f8207674452d8609124d237257d398377104
Miguel Barreto <miguel03barreto@gmail.com>
2026-09-16 18:36:31 -0300

    Update git clone command in builder.sh
    Removed branch specification from git clone command.

--- builder.sh
+++ builder.sh
@@ -18,7 +18,7 @@ git config --global user.name miguelbarretoo
 git config --global user.email miguel03barreto@gmail.com
 
 # Clone source
-git clone https://github.com/At30c/UN1CA_y2slte.git -b seventeen --recurse-submodules
+git clone https://github.com/At30c/UN1CA_y2slte.git --recurse-submodules
 
 # Remove sudo references:
 cd UN1CA_y2slte
